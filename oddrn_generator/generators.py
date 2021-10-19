c475ff0c98704a15dd11602a48f8dc0054b25b1e
Sofia Shnaidman <sshnaidman@provectus.com>
2021-10-19 13:32:21 +0300

    Add prefect generator.

--- oddrn_generator/generators.py
+++ oddrn_generator/generators.py
@@ -2,7 +2,7 @@ from oddrn_generator.path_models import (
     BasePathsModel, PostgresqlPathsModel, MysqlPathsModel, KafkaPathsModel, GluePathsModel, SnowflakePathsModel,
     AirflowPathsModel, HivePathsModel, DynamodbPathsModel, OdbcPathsModel,
     MssqlPathsModel, OraclePathsModel, RedshiftPathsModel, ClickHousePathsModel, KafkaConnectorPathsModel,
-    AthenaPathsModel, QuicksightPathsModel, DbtPathsModel
+    AthenaPathsModel, QuicksightPathsModel, DbtPathsModel, PrefectPathsModel
 )
 from oddrn_generator.server_models import AbstractServerModel, AWSCloudModel, HostnameModel
 
@@ -156,6 +156,11 @@ class QuicksightGenerator(Generator):
 class DbtGenerator(Generator):
     source = "dbt"
     paths_model = DbtPathsModel
+    server_model = HostnameModel
+
+class PrefectGenerator(Generator):
+    source = "prefect"
+    paths_model = PrefectPathsModel
     server_model = HostnameModel
 
 
