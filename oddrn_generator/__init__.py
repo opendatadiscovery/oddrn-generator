8fe1007aafdd46c9b641655cb72b73f5715fd4ca
Dmitrii Liubimov <dliubimov@provectus.com>
2021-11-03 10:18:32 +0300

    Neo4j generator implementation

--- oddrn_generator/__init__.py
+++ oddrn_generator/__init__.py
@@ -2,7 +2,7 @@ from oddrn_generator.generators import (
     Generator, PostgresqlGenerator, MysqlGenerator, KafkaGenerator, KafkaConnectGenerator, GlueGenerator,
     SnowflakeGenerator, AirflowGenerator, HiveGenerator, DynamodbGenerator, OdbcGenerator, MssqlGenerator,
     OracleGenerator, RedshiftGenerator, ClickHouseGenerator, AthenaGenerator, QuicksightGenerator, DbtGenerator,
-    TableauGenerator, PrefectGenerator
+    TableauGenerator, PrefectGenerator, Neo4jGenerator
 )
 
 __version__ = '0.1.13'
@@ -28,4 +28,5 @@ __all__ = [
     "DbtGenerator",
     "TableauGenerator",
     "PrefectGenerator",
+    "Neo4jGenerator",
 ]
