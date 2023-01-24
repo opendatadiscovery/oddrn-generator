b5512f160a35f6a230e276405225094b641eeb24
Pavel Mackarichev <vixtir90@gmail.com>
2023-01-24 15:32:09 +0300

    test: fix path

--- tests/test_filesystem_generator.py
+++ tests/test_filesystem_generator.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 from oddrn_generator.generators import FilesystemGenerator
+from oddrn_generator.utils import escape
 
 
 def test_file_system_generator():
     root_path = str(Path().cwd().resolve())
+
     generator = FilesystemGenerator(host_settings="local", path=root_path)
-    assert (
-        generator.get_oddrn_by_path("path")
-        == "//filesystem/host/local/path/\\\\Users\\\\pavelmakarichev\\\\Documents\\\\projects\\\\odd\\\\oddrn-generator"
-    )
+    assert generator.get_oddrn_by_path(
+        "path"
+    ) == "//filesystem/host/local/path/" + escape(root_path)
